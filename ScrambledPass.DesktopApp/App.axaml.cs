59120612b6ad618d20f4ddd032b2c1d50ad4f750
gstasikowski <stasikowski.gerard@tutanota.com>
2024-06-11 14:32:07 +0200

    View switching implementation

--- ScrambledPass.DesktopApp/App.axaml.cs
+++ ScrambledPass.DesktopApp/App.axaml.cs
@@ -18,14 +18,9 @@ public partial class App : Application
 	{
 		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
 		{
-			// desktop.MainWindow = new MainWindow
-			// {
-			//     DataContext = new MainWindowViewModel(),
-			// };
-
-			desktop.MainWindow = new GeneratorView
+			desktop.MainWindow = new MainWindow
 			{
-				DataContext = new GeneratorViewModel(),
+				DataContext = new MainWindowViewModel(),
 			};
 		}
 
