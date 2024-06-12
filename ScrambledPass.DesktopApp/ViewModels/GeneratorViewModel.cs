a2dedaa1fc24c8b20d2f77d0e00b6d3d094ba71b
gstasikowski <stasikowski.gerard@tutanota.com>
2024-06-12 21:03:42 +0200

    Implemented runtime localization, language selection from UI

--- ScrambledPass.DesktopApp/ViewModels/GeneratorViewModel.cs
+++ ScrambledPass.DesktopApp/ViewModels/GeneratorViewModel.cs
@@ -4,28 +4,6 @@ namespace ScrambledPass.DesktopApp.ViewModels;
 
 public class GeneratorViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-	public string EnableRandomWords => "Include random words";
-	public string WordCount => "Word count";
-	public string EnableRandomLetterSize => "Randomize letter size";
-	public string EnableRandomCharacters => "Include random characters";
-	public string UIReplaceSpacing => "Replace spacing";
-	public string UIReplaceCharacters => "Replace random characters";
-	public string UIRandomPosition => "Insert at random positions";
-	public string UICharacterCount => "Character count";
-	public string UIPassword => "Password";
-	public string UIGenerate => "Generate";
-	public string UIRandomize => "Randomize parameters";
-	public string UIEntropy => "Entropy";
-	public string Symbols => "Special characters";
-	public string SymbolMode => "Symbol mode";
-	public string UIPasswordStrength => "Password quality";
-	public string PasswordGreat => "great";
-	public string PasswordGood => "good";
-	public string PasswordWeak => "weak";
-	public string PasswordPoor => "poor";
-#pragma warning restore CA1822 // Mark members as static
-
 	private AppLogic? _appLogic;
 
 	public AppLogic? Generator
